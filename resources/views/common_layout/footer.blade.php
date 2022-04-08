8c6918e4dac425c42cbb590b4d68b1faef5fd39b
omarshohagdu <omarfaruk@du.ac.bd>
2022-04-08 10:08:03 +0600

    footer update

--- resources/views/common_layout/footer.blade.php
+++ resources/views/common_layout/footer.blade.php
@@ -29,7 +29,9 @@
     <div class="cus_con_footer">
       <div class="row">
         <div class="col-md-10 col-12 text-center">
-          <div style="color: #fff;margin-top: 3px;font-family: Tahoma;font-size:14px;">&copy; {{ date('Y') }} Natore Sadar.  Technical Support By : <a href="http://steptechbd.com/" style="color: #fff;font-weight: bold;" target="_blank">Step Technology</a></div>
+          <div style="color: #fff;margin-top: 3px;font-family: Tahoma;font-size:14px;">&copy; {{ date('Y') }} Design
+              and Developed By : <a href="http://steptechbd.com/" style="color: #fff;font-weight: bold;"
+                               target="_blank">Step Technology</a></div>
         </div>
         <div class="col-md-2 col-12" style="text-align: right;">
             <a href="http://steptechbd.com/" style="color: #fff;font-weight: bold;" target="_blank"><img src="{{ asset('fontView/assets/img/steptechIcon.png') }}" style="height: 30px;"></a>
